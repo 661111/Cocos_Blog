2f14c3814d1ff9c423d151e0e7bef8ad63a60e09
iKun <x1a2h1@qq.com>
2022-09-05 02:01:02 +0800

    14

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.INIS = {
     api: 'https://inisapi.crunl.cn/api',
     token: '47f8e3974a88cc31db69aec7904c262e',
-    route_hash: false,
-    version: '1.0.2'
+    route_hash: true,
+    version: '1.0.1'
 }
